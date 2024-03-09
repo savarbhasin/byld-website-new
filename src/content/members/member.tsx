b8b8625e4fa17b4a2e341240f3fe930ce9ef23b9
savarbhasin <savarbhasin446@gmail.com>
2024-03-09 22:00:00 +0530

    fix judges of devhaven

--- src/content/members/member.tsx
+++ src/content/members/member.tsx
@@ -12,34 +12,19 @@ export const team = {
           "skills":"HTML and Budget",
           "link":"https://github.com/Vikranth3140"
           },
-          {"name": "Harsh Mistry", "email": "harsh22200@iiitd.ac.in","image":harsh.src,"skills":"Flutter and Backend",
+          {"name": "Harsh Mistry", "email": "harsh22200@iiitd.ac.in","image":harsh.src,"skills":"Flutter and Flask",
         "link":"https://github.com/FakePickle" },
           {"name": "Armaan Singh", "email": "armaan22096@iiitd.ac.in","image":armaan.src,
         "skills": "Flutter and Selenium","link":"https://github.com/aturtle4"}
         ]
       },
       {
-        "role": "Dev Lead",
+        "role": "Leads",
         "members": [
-          {"name": "Vickey Kumar", "email": "vickey21299@iiitd.ac.in"}
-        ]
-      },
-      {
-        "role": "AI/ML Lead",
-        "members": [
-          {"name": "Argharupa Adhikary", "email": "argharupa23020@iiitd.ac.in"}
-        ]
-      },
-      {
-        "role": "Events & PR Lead",
-        "members": [
-          {"name": "Ekansh Khurana", "email": "ekansh21044@iiitd.ac.in"}
-        ]
-      },
-      {
-        "role": "Design Lead",
-        "members": [
-          {"name": "Devansh Kumar", "email": "devansh22152@iiitd.ac.in"}
+          {"name": "Vickey Kumar", "email": "vickey21299@iiitd.ac.in","lead":"Dev"},
+          {"name": "Argharupa Adhikary", "email": "argharupa23020@iiitd.ac.in","lead":"AI/ML"},
+          {"name": "Ekansh Khurana", "email": "ekansh21044@iiitd.ac.in","lead":"Events & PR"},
+          {"name": "Devansh Kumar", "email": "devansh22152@iiitd.ac.in","lead":"Design"}
         ]
       },
       {
@@ -50,57 +35,40 @@ export const team = {
         ]
       },
       {
-        "role": "Senior Web Developers",
+        "role": "Web Developers",
         "members": [
-          {"name": "Savar Bhasin", "email": "savar23497@iiitd.ac.in"},
-          {"name": "Avi Avinav", "email": "avi23159@iiitd.ac.in"}
+          {"name": "Savar Bhasin", "email": "savar23497@iiitd.ac.in","position":"Senior"},
+          {"name": "Avi Avinav", "email": "avi23159@iiitd.ac.in","position":"Senior"},
+          {"name": "Aaman Sheikh", "email": "aaman23006@iiitd.ac.in","position":"Junior"},
+          {"name": "Rahul Jha", "email": "rahul22389@iiitd.ac.in","position":"Junior"},
+          {"name": "Shreyansh Srivastav", "email": "shreyansh22485@iiitd.ac.in","position":"Junior"},
+          {"name": "Sanchit Maini", "email": "sanchit23480@iiitd.ac.in","position":"Junior"}
           
         ]
       },
       {
-        "role": "Senior App Developers",
+        "role": "App Developers",
         "members": [
-          {"name": "Harsh Aggarwal", "email": "harsh20508@iiitd.ac.in"},
-          {"name": "Rishabh Kumar", "email": "rishabh22402@iiitd.ac.in"},
-          {"name": "Lakshay Chauhan", "email": "lakshay21060@iiitd.ac.in"}
+          {"name": "Harsh Aggarwal", "email": "harsh20508@iiitd.ac.in","position":"Senior"},
+          {"name": "Rishabh Kumar", "email": "rishabh22402@iiitd.ac.in","position":"Senior"},
+          {"name": "Lakshay Chauhan", "email": "lakshay21060@iiitd.ac.in","position":"Senior"},
+          {"name": "Sushant Arora", "email": "sushant23537@iiitd.ac.in","position":"Junior"},
+          {"name": "Abhishek Sethi", "email": "abhishek23030@iiitd.ac.in","position":"Junior"},
+          {"name": "Aditya Gautam", "email": "aditya23043@iiitd.ac.in","position":"Junior"}
         ]
       },
       {
-        "role": "Senior AI/ML Developers",
+        "role": "AI/ML Developers",
         "members": [
-          {"name": "Ankush Paul", "email": "ankush22190@iiitd.ac.in"},
-          {"name": "Arunoday Ghorai", "email": "arunoday23023@iiitd.ac.in"},
-          {"name": "Sarthak Maini", "email": "sarthak20576@iiitd.ac.in"}
+          {"name": "Ankush Paul", "email": "ankush22190@iiitd.ac.in","position":"Senior"},
+          {"name": "Arunoday Ghorai", "email": "arunoday23023@iiitd.ac.in","position":"Senior"},
+          {"name": "Sarthak Maini", "email": "sarthak20576@iiitd.ac.in","position":"Senior"},
+          {"name": "Piyush Keshan", "email": "piyush23376@iiitd.ac.in","position":"Junior"},
+          {"name": "Arnesh Batra", "email": "arnesh23129@iiitd.ac.in","position":"Junior"},
+          {"name": "Krish Marwah", "email": "krish23288@iiitd.ac.in","position":"Junior"},
+          {"name": "Tejash Prasad", "email": "tejash22539@iiitd.ac.in","position":"Junior"}
         ]
       },
-      
-      {
-        "role": "Junior App Developers",
-        "members": [
-          {"name": "Sushant Arora", "email": "sushant23537@iiitd.ac.in"},
-          {"name": "Abhishek Sethi", "email": "abhishek23030@iiitd.ac.in"},
-          {"name": "Aditya Gautam", "email": "aditya23043@iiitd.ac.in"}
-        ]
-      },
-      {
-        "role": "Junior Web Developers",
-        "members": [
-          {"name": "Aaman Sheikh", "email": "aaman23006@iiitd.ac.in"},
-          {"name": "Rahul Jha", "email": "rahul22389@iiitd.ac.in"},
-          {"name": "Shreyansh Srivastav", "email": "shreyansh22485@iiitd.ac.in"},
-          {"name": "Sanchit Maini", "email": "sanchit23480@iiitd.ac.in"}
-        ]
-      },
-      {
-        "role": "Junior AI/ML Developers",
-        "members": [
-          {"name": "Piyush Keshan", "email": "piyush23376@iiitd.ac.in"},
-          {"name": "Arnesh Batra", "email": "arnesh23129@iiitd.ac.in"},
-          {"name": "Krish Marwah", "email": "krish23288@iiitd.ac.in"},
-          {"name": "Tejash Prasad", "email": "tejash22539@iiitd.ac.in"}
-        ]
-      },
-      
       {
         "role": "Design",
         "members": [
